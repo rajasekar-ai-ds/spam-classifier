44484f9b10b7fb1626e4806f0a9719d96b453ef9
Rajasekar P <p.rajasekar618240@gmail.com>
2025-09-16 21:09:51 +0530

    Update final_app.py

--- final_app.py
+++ final_app.py
@@ -14,12 +14,16 @@ st.sidebar.header("📌Menu")
 st.title("📨AI Email Spam Classifier")
 st.subheader("An AI app where you can check a mail/sms is spam or not")
 st.sidebar.title("Team members")
+st.sidebar.image("spamClassifier/logo_st.jpg")
 st.sidebar.markdown("""
 - Rajasekar P
 - Saravanan S
 - Raghav R K
 """)
 st.sidebar.markdown("---")
+
+st.sidebar.image("spamClassifier/pmist_logo.st.jpg")
+st.sidebar.success("Pmist , Vallam")
 st.sidebar.info("AI Spam Classifier built with scikit-learn and Streamlit....")
 
 
@@ -83,6 +87,7 @@ if option:
                                     unsafe_allow_html=True)
                     else:
                         st.success("NOT SPAM")
+
 
 
 
